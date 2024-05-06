102ac7a661c5c5479a6be2221b921c934fa667b1
Andrew Hale <andrew.hale003@gmail.com>
2024-05-06 19:45:37 +1000

    init

---
+++ src/test/java/com/example/restservice/HelloControllerIntegrationTests.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.restservice;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloControllerIntegrationTests {
+
+	@Autowired
+	private TestRestTemplate template;
+
+	@Test
+	public void getHello() throws Exception {
+		ResponseEntity<String> response = template.getForEntity("/", String.class);
+		assertThat(response.getBody()).isEqualTo("Hello World!");
+	}
+}
